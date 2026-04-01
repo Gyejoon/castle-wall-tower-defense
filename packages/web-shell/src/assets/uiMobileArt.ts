1192ca71f278e9e0ffb9c77f847ad0ac2bcaf113
Lio <lio@kmong.com>
2026-04-02 01:18:22 +0900

    perf: serve mobile lobby art as webp

--- packages/web-shell/src/assets/uiMobileArt.ts
+++ packages/web-shell/src/assets/uiMobileArt.ts
@@ -1,22 +1,22 @@
 export const uiMobileArt = {
-	lobbyKeyart: '/assets/ui-mobile/lobby-keyart-v20260329-b.png',
-	tacticalDockBackground: '/assets/ui-mobile/tactical-dock-bg-v20260329-a.png',
-	ctaPointArt: '/assets/ui-mobile/cta-point-art-v20260329-c.png',
-	courtyardBg: '/assets/ui-mobile/courtyard-bg-v20260331-a.png',
-	wartableBg: '/assets/ui-mobile/wartable-bg-v20260331-a.png',
-	lordchamberBg: '/assets/ui-mobile/lordchamber-bg-v20260331-a.png',
-	homeTabIconActive: '/assets/ui-mobile/home-tab-icon-active-v20260331-a.png',
+	lobbyKeyart: '/assets/ui-mobile/lobby-keyart-v20260329-b.webp',
+	tacticalDockBackground: '/assets/ui-mobile/tactical-dock-bg-v20260329-a.webp',
+	ctaPointArt: '/assets/ui-mobile/cta-point-art-v20260329-c.webp',
+	courtyardBg: '/assets/ui-mobile/courtyard-bg-v20260331-a.webp',
+	wartableBg: '/assets/ui-mobile/wartable-bg-v20260331-a.webp',
+	lordchamberBg: '/assets/ui-mobile/lordchamber-bg-v20260331-a.webp',
+	homeTabIconActive: '/assets/ui-mobile/home-tab-icon-active-v20260331-a.webp',
 	homeTabIconInactive:
-		'/assets/ui-mobile/home-tab-icon-inactive-v20260331-a.png',
+		'/assets/ui-mobile/home-tab-icon-inactive-v20260331-a.webp',
 	collectionTabIconActive:
-		'/assets/ui-mobile/collection-tab-icon-active-v20260331-a.png',
+		'/assets/ui-mobile/collection-tab-icon-active-v20260331-a.webp',
 	collectionTabIconInactive:
-		'/assets/ui-mobile/collection-tab-icon-inactive-v20260331-a.png',
+		'/assets/ui-mobile/collection-tab-icon-inactive-v20260331-a.webp',
 	settingsTabIconActive:
-		'/assets/ui-mobile/settings-tab-icon-active-v20260331-a.png',
+		'/assets/ui-mobile/settings-tab-icon-active-v20260331-a.webp',
 	settingsTabIconInactive:
-		'/assets/ui-mobile/settings-tab-icon-inactive-v20260331-a.png',
-	profileAvatar: '/assets/ui-mobile/profile-avatar-v20260331-a.png',
-	coinIcon: '/assets/ui-mobile/coin-icon-v20260331-a.png',
-	trophyIcon: '/assets/ui-mobile/trophy-icon-v20260331-a.png',
+		'/assets/ui-mobile/settings-tab-icon-inactive-v20260331-a.webp',
+	profileAvatar: '/assets/ui-mobile/profile-avatar-v20260331-a.webp',
+	coinIcon: '/assets/ui-mobile/coin-icon-v20260331-a.webp',
+	trophyIcon: '/assets/ui-mobile/trophy-icon-v20260331-a.webp',
 } as const;

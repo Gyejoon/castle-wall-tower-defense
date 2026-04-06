68d5cd1dbc6c6a0759b19f55bef542491ba48145
Lio <lio@kmong.com>
2026-04-06 19:40:10 +0900

    revert: 임무 탭 아이콘 v20260406-e로 복구

--- packages/web-shell/src/assets/uiMobileArt.ts
+++ packages/web-shell/src/assets/uiMobileArt.ts
@@ -17,9 +17,9 @@ export const uiMobileArt = {
 	settingsTabIconInactive:
 		'/assets/ui-mobile/settings-tab-icon-inactive-v20260331-a.webp',
 	missionTabIconActive:
-		'/assets/ui-mobile/mission-tab-icon-active-v20260406-g.png',
+		'/assets/ui-mobile/mission-tab-icon-active-v20260406-e.png',
 	missionTabIconInactive:
-		'/assets/ui-mobile/mission-tab-icon-inactive-v20260406-h.png',
+		'/assets/ui-mobile/mission-tab-icon-inactive-v20260406-e.png',
 	profileAvatar: '/assets/ui-mobile/profile-avatar-v20260331-a.webp',
 	coinIcon: '/assets/ui-mobile/coin-icon-v20260331-a.webp',
 	trophyIcon: '/assets/ui-mobile/trophy-icon-v20260331-a.webp',
